78321645105146273549a0d09689ce7f4ed334c7
rhadiyyaharn <radiyahariana@gmail.com>
2023-11-01 23:17:12 +0700

    update dashboard doswal

--- resources/views/dashboardDosen.blade.php
+++ resources/views/dashboardDosen.blade.php
@@ -20,7 +20,6 @@
                         <h6 class="align-items-center"><i class="bi bi-backpack-fill"></i> Mahasiswa</h6>
                     </div>
                 </div>
-
             </div>
         </div>
     </section>
@@ -30,6 +29,29 @@
             <h1 class="display-6">Daftar Mahasiswa</h1>
         </div>
 
+
+        <form class="d-flex" role="search">
+            <select id="filterAngkatan" class="form-select me-2">
+                <option value="" selected>Semua Angkatan</option>
+                <option value="2019">2019</option>
+                <option value="2020">2020</option>
+                <option value="2021">2021</option>
+                <option value="2022">2022</option>
+                <option value="2023">2023</option>
+            </select>
+        </form>
+
+        <div class="navbar bg-body-tertiary justify-content-end">
+            <div class="container-fluid">
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" id="searchInput"
+                        placeholder="Cari mahasiswa berdasarkan nama" aria-label="Search">
+                    <button class="btn btn-outline-success" type="button" onclick="searchMahasiswa()">Search</button>
+                </form>
+            </div>
+        </div>
+        <div id="searchError" class="alert alert-danger mt-2" style="display: none;"></div> <!-- Perubahan ini -->
+
         <div class="container-lg my-5 pb-4">
             @if (session('status'))
                 <div class="alert alert-success alert-dismissible fade show" role="alert">
@@ -37,6 +59,7 @@
                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                 </div>
             @endif
+
             <div class="justify-content-center">
                 <table class="table table-hover table-bordered">
                     <thead class="table-dark">
@@ -48,7 +71,7 @@
                             <th scope="col">Aksi</th>
                         </tr>
                     </thead>
-                    <tbody>
+                    <tbody id="mahasiswaTable">
                         @foreach ($mahasiswas as $mahasiswa)
                             <tr>
                                 <td>{{ $mahasiswa->nama }}</td>
@@ -56,19 +79,64 @@
                                 <td>{{ $mahasiswa->angkatan }}</td>
                                 <td>{{ $mahasiswa->status }}</td>
                                 <td>
-                                    <button class="btn btn-warning btn-sm">Edit</button>
-                                    <button class="btn btn-danger btn-sm">Delete</button>
+                                    <button class="btn btn-warning btn-sm">Detail</button>
                                 </td>
                             </tr>
                         @endforeach
                     </tbody>
                 </table>
             </div>
-
-            <div class="d-flex justify-content-end mt-4">
-                <a href="/mahasiswa-create" class="btn btn-primary">+ Tambah Mahasiswa</a>
-                <!-- Ganti "/tambah-mahasiswa" dengan URL yang sesuai -->
-            </div>
         </div>
+
+        <script>
+            function searchMahasiswa() {
+                var input = document.getElementById('searchInput').value.toLowerCase();
+                var table = document.getElementById('mahasiswaTable');
+                var rows = table.getElementsByTagName('tr');
+                var errorDiv = document.getElementById('searchError');
+
+                errorDiv.style.display = 'none'; // Sembunyikan pesan kesalahan
+
+                for (var i = 0; i < rows.length; i++) {
+                    var nama = rows[i].getElementsByTagName('td')[0].textContent.toLowerCase();
+                    if (nama.includes(input)) {
+                        rows[i].style.display = '';
+                    } else {
+                        rows[i].style.display = 'none';
+                    }
+                }
+
+                // Tampilkan pesan kesalahan jika tidak ada hasil yang sesuai
+                if ([...rows].every(row => row.style.display === 'none')) {
+                    errorDiv.innerHTML = 'Nama tidak ada di dalam daftar mahasiswa';
+                    errorDiv.style.display = 'block';
+                }
+            }
+
+            function searchMahasiswa() {
+                var input = document.getElementById('searchInput').value.toLowerCase();
+                var filterAngkatan = document.getElementById('filterAngkatan').value;
+                var table = document.getElementById('mahasiswaTable');
+                var rows = table.getElementsByTagName('tr');
+                var errorDiv = document.getElementById('searchError');
+
+                errorDiv.style.display = 'none';
+
+                for (var i = 0; i < rows.length; i++) {
+                    var nama = rows[i].getElementsByTagName('td')[0].textContent.toLowerCase();
+                    var angkatan = rows[i].getElementsByTagName('td')[2].textContent;
+                    if (nama.includes(input) && (filterAngkatan === "" || angkatan === filterAngkatan)) {
+                        rows[i].style display = '';
+                    } else {
+                        rows[i].style.display = 'none';
+                    }
+                }
+
+                if ([...rows].every(row => row.style.display === 'none')) {
+                    errorDiv.innerHTML = 'Nama tidak ada di dalam daftar mahasiswa';
+                    errorDiv.style.display = 'block';
+                }
+            }
+        </script>
     </section>
 @endsection
