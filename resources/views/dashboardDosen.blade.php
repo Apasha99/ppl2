9d518c2d6e14490fed5d33c4e741c77c4b87d593
rhadiyyaharn <radiyahariana@gmail.com>
2023-11-01 22:20:34 +0700

    update

--- resources/views/dashboardDosen.blade.php
+++ resources/views/dashboardDosen.blade.php
@@ -1,11 +1,74 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard Dosen</title>
-</head>
-<body>
-    ini dashboard dosen
-</body>
-</html>
+@extends('layouts.layoutOperator')
+
+@section('content')
+    <section>
+        <div class="container-lg my-5">
+            <div class="text-center text-light">
+                <div class="display-6">Welcome, {{ Auth::user()->username }}</div>
+            </div>
+
+            <div class="row justify-content-center align-items-center gy-3 mt-5">
+                <div class="col-lg-3 mx-2 bg-light border border-dark border-2 rounded">
+                    <div class="d-flex justify-content-between pt-3 pe-2">
+                        <p class="border border-dark border-1 rounded px-3">{{ $mahasiswaperwalian_count }}</p>
+                        <h6 class="align-items-center"><i class="bi bi-mortarboard-fill"></i> Mahasiswa Perwalian</h6>
+                    </div>
+                </div>
+                <div class="col-lg-3 mx-2 bg-light border border-dark border-2 rounded">
+                    <div class="d-flex justify-content-between pt-3 pe-2">
+                        <p class="border border-dark border-1 rounded px-3">{{ $mahasiswaCount }}</p>
+                        <h6 class="align-items-center"><i class="bi bi-backpack-fill"></i> Mahasiswa</h6>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <section class="bg-light rounded">
+        <div class="text-center my-5 pt-5">
+            <h1 class="display-6">Daftar Mahasiswa</h1>
+        </div>
+
+        <div class="container-lg my-5 pb-4">
+            @if (session('status'))
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    <i class="bi bi-person-fill"></i> {{ session('status') }}
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            @endif
+            <div class="justify-content-center">
+                <table class="table table-hover table-bordered">
+                    <thead class="table-dark">
+                        <tr>
+                            <th scope="col">Nama</th>
+                            <th scope="col">NIM</th>
+                            <th scope="col">Angkatan</th>
+                            <th scope="col">Status</th>
+                            <th scope="col">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($mahasiswas as $mahasiswa)
+                            <tr>
+                                <td>{{ $mahasiswa->nama }}</td>
+                                <td>{{ $mahasiswa->nim }}</td>
+                                <td>{{ $mahasiswa->angkatan }}</td>
+                                <td>{{ $mahasiswa->status }}</td>
+                                <td>
+                                    <button class="btn btn-warning btn-sm">Edit</button>
+                                    <button class="btn btn-danger btn-sm">Delete</button>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="d-flex justify-content-end mt-4">
+                <a href="/mahasiswa-create" class="btn btn-primary">+ Tambah Mahasiswa</a>
+                <!-- Ganti "/tambah-mahasiswa" dengan URL yang sesuai -->
+            </div>
+        </div>
+    </section>
+@endsection
