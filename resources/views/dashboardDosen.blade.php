83fa446b78e0508a162e67818c6bf05cc4c654c6
rhadiyyaharn <radiyahariana@gmail.com>
2023-11-07 22:23:23 +0700

    update

--- resources/views/dashboardDosen.blade.php
+++ resources/views/dashboardDosen.blade.php
@@ -38,7 +38,8 @@
                 <div class="col-lg-3 mx-2 bg-light border border-dark border-2 rounded">
                     <div class="d-flex justify-content-between pt-3 pe-2">
                         <p class="border border-dark border-1 rounded px-3 text-dark">{{ $mahasiswaPerwalianCount }}</p>
-                        <h6 class="align-items-center text-dark"><i class="bi bi-mortarboard-fill"></i> Mahasiswa Perwalian</h6>
+                        <h6 class="align-items-center text-dark"><i class="bi bi-mortarboard-fill"></i> Mahasiswa Perwalian
+                        </h6>
                     </div>
                 </div>
                 <div class="col-lg-3 mx-2 bg-light border border-dark border-2 rounded">
@@ -58,7 +59,8 @@
         <div class="col-lg-10 mx-10">
             <form action="{{ route('searchMhs') }}" method="GET">
                 <label for="search" style="color:#000">Cari Mahasiswa:</label>
-                <input type="text" class="input input-bordered w-full" id="search" name="search" value="{{ request('search') }}" placeholder="Cari berdasarkan nama/nim/angkatan/status">
+                <input type="text" class="input input-bordered w-full" id="search" name="search"
+                    value="{{ request('search') }}" placeholder="Cari berdasarkan nama/nim/angkatan/status">
                 <button type="submit" class="btn btn-primary w-full">Cari</button>
             </form>
         </div>
@@ -107,24 +109,25 @@
                 var table = document.getElementById('mahasiswaTable');
                 var rows = table.getElementsByTagName('tr');
                 var errorDiv = document.getElementById('searchError');
-        
+
                 errorDiv.style.display = 'none';
-        
+
                 var resultFound = false;
-        
+
                 for (var i = 0; i < rows.length; i++) {
                     var nama = rows[i].getElementsByTagName('td')[0].textContent.toLowerCase();
                     var nim = rows[i].getElementsByTagName('td')[1].textContent; // Dapatkan NIM dari kolom ke-2
                     var angkatan = rows[i].getElementsByTagName('td')[2].textContent;
-        
-                    if ((nama.includes(input) || nim.includes(input)) && (filterAngkatan === "" || angkatan === filterAngkatan)) {
+
+                    if ((nama.includes(input) || nim.includes(input)) && (filterAngkatan === "" || angkatan ===
+                            filterAngkatan)) {
                         rows[i].style.display = '';
                         resultFound = true;
                     } else {
                         rows[i].style.display = 'none';
                     }
                 }
-        
+
                 if (!resultFound) {
                     errorDiv.innerHTML = 'Nama atau NIM tidak ada di dalam daftar mahasiswa';
                     errorDiv.style.display = 'block';
@@ -132,7 +135,7 @@
                     errorDiv.style.display = 'none';
                 }
             }
-        </script>        
+        </script>
 
     </section>
 @endsection
